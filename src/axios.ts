9178b1272de80cc06932de97b44d547d7d5ecd54
leangsengLy <edeepleang@gmail.com>
2023-02-28 06:42:59 +0700

    add axios

--- src/axios.ts
+++ src/axios.ts
@@ -3,12 +3,24 @@ import { base_url } from './base.url';
 const getToken = ()=>{
         return localStorage.getItem('token');
 }
+// axios Syntax and use it work
+// const AxiosInstance = axios.create({
+//         baseURL: base_url,
+//         timeout: 1000,
+//         headers: {
+//                 Authorization:"Brearer "+ getToken()
+//         }
+// });
+
+
+// axios Syntax and use 
 const AxiosInstance = axios.create({
         baseURL: base_url,
         timeout: 1000,
-        headers: {
-                Authorization:"Brearer "+ getToken()
-        }
 });
+AxiosInstance.interceptors.request.use(function(config:any){
+        config.headers.Authorization = 'Bearer ' + getToken();
+        return config;
+})
 export { AxiosInstance };
       
